160589d552b1e3e43eeb98ece53595870c870fa4
Victor <pipoca.play19@gmail.com>
2024-10-07 18:33:01 -0300

    v 1.5

---
+++ src/main/java/com/example/demo/controller/DeleteMachineByID.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.MaquinaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+
+public class DeleteMachineByID {
+
+    private final MaquinaService maquinaService;
+    @Autowired
+    public DeleteMachineByID(MaquinaService maquinaService) {
+        this.maquinaService = maquinaService;
+    }
+    @CrossOrigin("*")
+    @DeleteMapping("/maquinas/delete/{id}")
+    public ResponseEntity<?> deleteMachineByID(@PathVariable Long id) {
+        boolean deletado;
+        deletado = maquinaService.deleteMaquina(id);
+        if (deletado){
+            return ResponseEntity.ok().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
